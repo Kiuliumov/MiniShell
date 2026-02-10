fe5be901ef48793b2104b8a67f619f8a7c204637
Kiuliumov <miti.bania@gmail.com>
2026-02-10 14:13:30 +0200

    Update minishell.c

--- minishell.c
+++ minishell.c
@@ -8,11 +8,12 @@ int main(void)
     char *line = NULL;
     size_t len = 0;
     char *args[100];
-    int i = 0;
+    int i;
     int EOS = -1;
 
     while (1)
     {
+        i = 0;
         write(1, "$ ", 2);
 
         if (getline(&line, &len, stdin) == EOS)
@@ -21,6 +22,38 @@ int main(void)
         }
 
         line[strcspn(line, "\n")] = '\0';
+
+        char *token = strtok(line, " ");
+
+        while (token != NULL)
+        {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        if (args[0] != NULL)
+        {
+            pid_t pid = fork();
+
+            if (pid == 0)
+            {
+                if (execvp(args[0], args) == -1)
+                {
+                    perror("shell");
+                }
+                exit(EXIT_FAILURE);
+            }
+            else if (pid < 0)
+            {
+                perror("shell");
+            }
+
+            else
+            {
+                int status;
+                waitpid(pid, &status, 0);
+            }
+        }
     }
 
     free(line);
