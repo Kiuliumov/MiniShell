b8febfa7b36c246df9dfaa3c11731452583fe3cf
Kiuliumov <miti.bania@gmail.com>
2026-02-10 12:23:35 +0200

    Update minishell.c

--- minishell.c
+++ minishell.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(void)
 {
+    char *line = NULL;
+    size_t len = 0;
+
     while (1)
     {
         write(1, "$ ", 2);
+
+        if (getline(&line, &len, stdin) == -1)
+        {
+            break;
+        }
+
         pause();
     }
     return 0;
